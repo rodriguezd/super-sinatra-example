f0e24a5279e26eec0abbbb656f88828a6e097905
David Rodriguez <davidrodriguez212@gmail.com>
2013-06-27 18:16:11 -0400

    Done

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ module SuperSinatra
   class App < Sinatra::Base
 
     get '/' do
+      erb :super_hero
+    end
+
+    post '/team' do
+
+      # raise params.inspect
+      @team = params[:team]
+      erb :team
     end
 
   end
